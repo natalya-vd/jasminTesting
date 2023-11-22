a67a52c00a62a5850535f704f5b3bf7f78f2c5b9
Natalya <natasha-vd@yandex.ru>
2023-11-22 13:33:50 +0300

    add expectations

--- calculator.spec.js
+++ calculator.spec.js
@@ -1,4 +1,19 @@
 // describe - набор тестов
 describe('Calculator.js', () => {
-  // test
+  //it - Описание одного теста
+  it('should add number to the total', () => {
+    // TODO: expectations
+  })
+
+  it('should subtract number from total', () => {
+    // TODO: expectations
+  })
+
+  it('should multiply number with the total', () => {
+    // TODO: expectations
+  })
+
+  it('should divide number by the total', () => {
+    // TODO: expectations
+  })
 })
