f640f98720b85434a36f85758f50ef314c9a559f
Natalya <natasha-vd@yandex.ru>
2023-11-23 11:19:40 +0300

    spy: create spy for function

--- main.spec.js
+++ main.spec.js
@@ -1,6 +1,12 @@
 describe('main.js', () => {
   describe('calculate()', () => {
-    xit('validate expression')
+    it('validate expression if the number is invalid', () => {
+      // stub - заглушка. Метод updateResult на самом деле не будет вызван, но дальше передастся как будто он вызывался
+      spyOn(window, 'updateResult').and.stub()
+
+      calculate('a+3')
+
+    })
     xit('calls add')
     xit('calls subtract')
     xit('calls multiply')
